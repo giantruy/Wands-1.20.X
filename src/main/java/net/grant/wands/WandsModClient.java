544235328f9a13fb02075473a20a081dc3c7c552
CinnamonRoll <grantyiu@proton.me>
2025-04-25 00:09:35 -0700

    Initial commit

---
+++ src/main/java/net/grant/wands/WandsModClient.java
@@ -0,0 +1,10 @@
+package net.grant.wands;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WandsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/grant/wands/WandsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.grant.wands;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WandsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
